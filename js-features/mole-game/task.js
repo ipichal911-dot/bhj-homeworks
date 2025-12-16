00cd936f479943e751ac659202c2f55c9926d124
Pichal <ipichal@yandex.ru>
2025-12-16 09:05:00 +0300

    mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,42 @@
+const getHole = index => document.getElementById(`hole${index}`);
+
+const deadCounter = document.getElementById('dead');
+const lostCounter = document.getElementById('lost');
+
+let dead = 0;
+let lost = 0;
+
+const resetGame = () => {
+  dead = 0;
+  lost = 0;
+  deadCounter.textContent = dead;
+  lostCounter.textContent = lost;
+}
+
+const checkGameStatus = () => {
+  if (dead >= 10) {
+    alert('Победа!');
+    resetGame();
+  } else if (lost >= 5) {
+    alert('Вы проиграли!');
+    resetGame();
+  }
+}
+
+for (let i = 1; i <= 9; i++) {
+  const hole = getHole(i);
+
+  hole.onclick = function() {
+    
+    if (hole.className.includes('hole_has-mole')) {
+      dead++;
+      deadCounter.textContent = dead;
+    } else {
+      lost++;
+      lostCounter.textContent = lost;
+    }
+    
+    checkGameStatus();
+  }
+}
+
